8e9d0cc66a68d3b5c66fd186e17ba5e38a76a455
MateuszSabuk <mateusz@sabuk.pl>
2024-10-08 13:20:08 +0200

    Implemented crude gui

--- ZippGUI/MainWindow.xaml.cs
+++ ZippGUI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Shapes;
 using CLI;
 
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection.PortableExecutable;
 
 namespace ZippGUI
 {
@@ -20,11 +23,91 @@ namespace ZippGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        SolverManager manager;
         public MainWindow()
         {
+            manager = new SolverManager();
             InitializeComponent();
-            SolverManager manager = new SolverManager();
-            Tuple<int[],int[,]> a = manager.generate(4, 10, 5, 20);
+            // Set default values for the inputs
+            numOfStages.Text = "4";
+            numOfTasks.Text = "10";
+            maxNumOfMachinesInStage.Text = "5";
+            maxTaskTime.Text = "20";
+        }
+
+        private void GenerateButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int s = int.Parse(numOfStages.Text);
+                int n = int.Parse(numOfTasks.Text);
+                int maxMIS = int.Parse(maxNumOfMachinesInStage.Text);
+                int maxTT = int.Parse(maxTaskTime.Text);
+                Tuple<int[], int[,]> _ = manager.generate(s, n, maxMIS, maxTT);
+                GeneratedText.Text = tuple2string(_);
+                // TODO save it and display nicely
+            }
+            catch (Exception ex)
+            {
+                // TODO Exception handling
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]+$");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void AlgorithmChoice_Initialized(object sender, EventArgs e)
+        {
+            string[] algorithmNames = manager.getAlgorithmNames();
+
+            AlgorithmChoice.ItemsSource = algorithmNames;
+            if (algorithmNames.Length > 0)
+            {
+                AlgorithmChoice.SelectedIndex = 0;
+            }
+        }
+
+        private void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int algId = AlgorithmChoice.SelectedIndex;
+            //manager.run(algId, ...);
+        }
+
+        private string tuple2string(Tuple<int[], int[,]> instance)
+        {
+            // Extract machines and taskTimes from the Tuple
+            int[] machines = instance.Item1;
+            int[,] taskTimes = instance.Item2;
+
+            // Initialize result string
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("Instance object:");
+            str.AppendLine("Stages:");
+
+            // Convert machines (1D array) to string
+            str.Append("[");
+            for (int i = 0; i < machines.Length - 1; i++)
+            {
+                str.Append(machines[i].ToString() + ", ");
+            }
+            str.Append(machines[machines.Length - 1].ToString() + "]\n");
+
+            // Convert taskTimes (2D array) to string
+            str.AppendLine("Tasks:");
+            for (int i = 0; i < taskTimes.GetLength(0); i++)
+            {
+                str.Append("[");
+                for (int j = 0; j < taskTimes.GetLength(1) - 1; j++)
+                {
+                    str.Append(taskTimes[i, j].ToString() + ", ");
+                }
+                str.Append(taskTimes[i, taskTimes.GetLength(1) - 1].ToString() + "]\n");
+            }
+
+            return str.ToString();
         }
     }
 }
