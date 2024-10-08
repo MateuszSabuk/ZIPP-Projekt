f28e04ff3bcabad0eacccbdf1d74d5217c5a3a6b
MateuszSabuk <mateusz@sabuk.pl>
2024-10-08 19:37:52 +0200

    Made display look nicer

--- ZippAlgorithmsLibrary/Bruteforce.cpp
+++ ZippAlgorithmsLibrary/Bruteforce.cpp
@@ -3,6 +3,9 @@
 Bruteforce::Bruteforce() : Algorithm("Bruteforce")
 {
 	parameters.insert({ "Test parameter" , 1 });
+	parameters.insert({ "Test prameter" , 42 });
+	parameters.insert({ "Test paramter" , 5 });
+	parameters.insert({ "Test paraeter" , 1 });
 }
 
 std::vector<std::vector<std::pair<int, int>>> Bruteforce::run(std::vector<int> machines, std::vector<std::vector<int>> taskTimes)
