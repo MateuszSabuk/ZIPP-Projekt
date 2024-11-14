d176fb5fa7e9abf58dfd86e9f85518e0647120e0
MateuszSabuk <mateusz@sabuk.pl>
2024-11-14 01:47:00 +0100

    Fixed Quickperm

--- ZippAlgorithmsLibrary/Bruteforce.cpp
+++ ZippAlgorithmsLibrary/Bruteforce.cpp
@@ -15,7 +15,7 @@ std::vector<std::vector<std::pair<int, int>>> Bruteforce::start(const std::vecto
 	int minTime = INT_MAX;
 	int time;
 	std::vector<int> minPermutationNumbers;
-	
+
 	for (auto permutationNumbers : quickPerm(size(taskTimes)))
 	{
 		auto permutationSchedule = create_schedule(time, machines, taskTimes, permutationNumbers);
@@ -31,32 +31,31 @@ std::vector<std::vector<std::pair<int, int>>> Bruteforce::start(const std::vecto
 
 std::vector<std::vector<int>> Bruteforce::quickPerm(int length)
 {
-	std::vector<std::vector<int>> out;
-	auto a = std::vector<int>(length);
-	auto p = std::vector<int>(length + 1);
+    std::vector<std::vector<int>> out;
+    auto a = std::vector<int>(length);
+    auto p = std::vector<int>(length + 1);
 
-	for (int i = 0; i < length; i++)
-	{
-		if (isCanceled) throw "canceled";
+    for (int i = 0; i < length; i++)
+    {
+        if (isCanceled) throw "canceled";
 		a[i] = p[i] = i;
-	}
-	p[length] = length;
-	out.push_back(a);
+    }
+    out.push_back(a);
 
-	int i = 0;
-	while (i < length)
-	{
-		if (isCanceled) throw "canceled";
-		--p[i];
+    int i = 0;
+    while (i < length)
+    {
+        if (isCanceled) throw "canceled";
+        --p[i];
 		int j = i % 2 * p[i];
-		std::swap(a[i], a[j]);
-		out.push_back(a);
-		i = 1;
+        std::swap(a[i], a[j]);
+        out.push_back(a);
+        i = 1;
 		while (!p[i])
-		{
-			p[i] = 1;
-			i++;
-		}
-	}
-	return out;
+        {
+            p[i] = i;
+            i++;
+        }
+    }
+    return out;
 }
